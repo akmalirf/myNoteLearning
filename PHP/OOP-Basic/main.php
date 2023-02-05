8173e575a1e089009baaeaa66cafea1c838e6427
akmalirf <akmalirfansyah31@gmail.com>
2023-02-05 22:55:16 +0700

    update

---
+++ PHP/OOP-Basic/main.php
@@ -0,0 +1,69 @@
+<?php
+
+// how to make a class
+
+class Fruit {
+  // This is a Field/Property/Attributes
+  public $name;
+  public $color;
+
+  // This is a Method/Function/Behavior
+  function introduce(){
+    return $this->name . " " . $this->color;
+  }
+}
+
+// How to make object
+$apple = new Fruit();
+$apple -> name = "apel";
+$apple -> color= "merah";
+
+echo $apple->introduce();
+// Result Apel Merah
+
+?>
+
+<!-- Encapsulation 
+  The wrapping up of data and methods into a single unit (called class) is known as encapsulation. 
+  Encapsulation is a protection mechanism for the data members and methods present inside the class. 
+  In the encapsulation technique, we are restricting the data members from access to outside world end-user.
+
+  In PHP, encapsulation utilized to make the code more secure and robust. 
+  Using encapsulation, we are hiding the real implementation of data from the user and also does not 
+  allow anyone to manipulate data members except by calling the desired operation.
+-->
+
+  <!-- Encapsulation Implementation -->
+
+<?php
+
+class Car {
+  private $name;
+  private $color;
+
+  // Private mean property cant be accces from outside class, only use method to manipulate property
+
+  function set_name($name){
+    return $this->name = $name;
+  }
+  function get_name(){
+    return $this->name;
+  }
+  function set_color($color){
+    return $this->color = $color;
+  }
+  function get_color(){
+    return $this->color;
+  }
+  function introduce(){
+    return $this->name . " " . $this->color;
+  }
+}
+
+$sedan = new car();
+$sedan->set_name("honda");
+$sedan->set_color("hitam");
+
+echo $sedan->introduce();
+// Result Honda Hitam
+?>
